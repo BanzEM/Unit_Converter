b00aa609b7a5010d135ec058db28b04054bd2a17
BanzEM <bandisilemazomba123@gmail.com>
2026-03-02 22:01:17 +0200

    Initial Commit

---
+++ Unit_Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit_Converter
+//
+//  Created by Bandisile Mazomba on 2026/03/02.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
