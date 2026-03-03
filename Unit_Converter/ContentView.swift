d9a55eddbf558351272826a56b409ed345dfa565
BanzEM <bandisilemazomba123@gmail.com>
2026-03-03 13:55:21 +0200

    added celcius textfield and kelvin text

--- Unit_Converter/ContentView.swift
+++ Unit_Converter/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var unit: Double = 0.0
-    @State private var convertedUnit: Double = 273.15
+    @State private var celciusUnit =  0.0
+    @State private var convertedUnit =  273.15
     
     var body: some View {
         NavigationStack{
             Form{
+                Section("Temperature in celcius"){
+                    TextField("Enter value in Celcius", value: $celciusUnit, format: .number )
+                }
                 
-                
+                Section("Temperature in Kelvin"){
+                    Text("\(convertedUnit, specifier: "%.2f")")
+                }
                 
             }.navigationTitle("Unit Converter")
                 .navigationBarTitleDisplayMode(.inline)
